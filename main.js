f3a8675b8e395161c4b26de3c4ba88c2fabc6aaf
Camilla Policarpo <policarpofront@gmail.com>
2024-04-05 13:34:34 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const nomeDaAtividade = $('input').val();
+        const novaAtividade = $(`<li>${nomeDaAtividade}</li>`)
+
+        $(novaAtividade).appendTo('ul')
+        $('input').val('')
+    })
+
+    $('ul').on('click', 'li', function(e){
+        e.preventDefault();
+        $(this).toggleClass('completed')
+
+    })
+
+})
